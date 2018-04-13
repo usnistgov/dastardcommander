56776269821db68c38ddd8c909118cde3cface89
Joe Fowler <drjoefowler@gmail.com>
2018-04-13 15:35:47 -0600

    Add a basic RPC client. Not sure this works yet.

---
+++ rpc_client.py
@@ -0,0 +1,40 @@
+import json
+
+import itertools, socket
+
+class JSONClient(object):
+
+    def __init__(self, addr, codec=json):
+        self._socket = socket.create_connection(addr)
+        self._id_iter = itertools.count()
+        self._codec = codec
+
+    def _message(self, name, params):
+        return dict(id=next(self._id_iter),
+                    params=[params],
+                    method=name)
+
+    def call(self, name, params):
+        request = self._message(name, params)
+        print "jwf sending request :<%s>"%request
+        id = request.get('id')
+        msg = self._codec.dumps(request)
+        self._socket.sendall(msg.encode())
+
+        # This will actually have to loop if resp is bigger
+        response = self._socket.recv(4096)
+        response = self._codec.loads(response.decode())
+
+        if response.get('id') != id:
+            raise Exception("expected id=%s, received id=%s: %s"
+                            %(id, response.get('id'),
+                              response.get('error')))
+
+        if response.get('error') is not None:
+            print "Yikes! Reponse is: ", response
+            raise Exception(response.get('error'))
+
+        return response.get('result')
+
+    def close(self):
+        self._socket.close()
