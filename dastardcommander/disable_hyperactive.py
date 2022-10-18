54a1f5b3e35b07f505bd9da351ec61e698c48d54
Joe Fowler <drjoefowler@gmail.com>
2022-10-18 17:33:53 -0600

    Rewrite disable hyperactive to do work in QThread

--- dastardcommander/disable_hyperactive.py
+++ dastardcommander/disable_hyperactive.py
@@ -31,29 +31,60 @@ class DisableHyperDialog(QtWidgets.QDialog):
         self.levelSpinBox.setDisabled(True)
         self.startButton.setDisabled(True)
 
-        positive = self.positivePulseButton.isChecked()
-        signmessage = "falling"
-        if positive:
-            signmessage = "rising"
-        threshold = self.levelSpinBox.value()
-
         self.textBrowser.clear()
         self.cursor = self.textBrowser.textCursor()
-        self.cursor.insertText(f"Configuring {signmessage} edge triggers at threshold {threshold} ...\n")
+
+        self.thread = QtCore.QThread()
+        positive = self.positivePulseButton.isChecked()
+        threshold = self.levelSpinBox.value()
+        self.worker = DisableHyperWorker(self.dcom, positive, threshold)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.worker.message.connect(self.reportProgress)
+        self.thread.start()
+
+    @pyqtSlot(str)
+    def reportProgress(self, message):
+        self.cursor.insertText(message)
+
+
+class DisableHyperWorker(QtCore.QObject):
+    """QObject that can run in a QThread to keep GUI (main) thread responsive."""
+
+    finished = pyqtSignal()
+    message = pyqtSignal(str)
+
+    def __init__(self, dcom, positive, threshold):
+        self.dcom = dcom
+        self.positive = positive
+        self.threshold = threshold
+        super().__init__()
+
+    def run(self):
+        signmessage = "falling"
+        if self.positive:
+            signmessage = "rising"
+        self.message.emit(f"Configuring {signmessage} edge triggers at threshold {self.threshold} ...\n")
+
         self.save_quiet = "TRIGGER" in self.dcom.quietTopics
         if not self.save_quiet:
             self.dcom.quietTopics.add("TRIGGER")
             print("Will suppress printing of TRIGGER status until disabling hyperactive channels is complete.")
 
-        self.cursor.insertText("1) Stopping all triggers.\n")
+        self.message.emit("1) Stopping all triggers.\n")
         if not self.zeroAllTriggers():
-            self.cursor.insertText("X  Failed: no channels known.\n")
+            self.message.emit("X  Failed: no channels known.\n")
+            self.finished.emit()
             return
 
-        self.cursor.insertText("2) Turning on edge triggers.\n")
+        self.message.emit("2) Turning on edge triggers.\n")
         channels_to_configure = self.dcom.triggerTabSimple.channelIndicesSignalOnly(exclude_blocked=True)
         if not self.startEdgeTriggers(channels_to_configure):
-            self.cursor.insertText("X  Failed: no channels known.\n")
+            self.message.emit("X  Failed: no channels known.\n")
+            self.finished.emit()
             return
 
         # 3) Collect trigger rate data
@@ -62,21 +93,29 @@ class DisableHyperDialog(QtWidgets.QDialog):
         duration = 0.0
         t0 = time.time()
         integration_time = 5.0
-        self.cursor.insertText(f"3) Collecting trigger rate data (takes ~{integration_time} seconds).\n")
+        self.message.emit(f"3) Collecting trigger rate data (takes ~{integration_time} seconds).\n")
 
         while True:
             time.sleep(0.25)
             triggerRateMessageID, msg = self.dcom.lastTriggerRateMessage
             if triggerRateMessageID != staleTriggerRateMessageID:
                 triggerRateMessageID = staleTriggerRateMessageID
-                if "CountsSeen" in msg:
+                if "CountsSeen" in msg and "Duration" in msg:
                     trigcounts += msg["CountsSeen"]
                     duration += msg["Duration"]
 
             if time.time() - t0 > integration_time:
                 break
 
+        duration /= 1e9  # convert ns to seconds
+        if duration <= 0:
+            self.message.emit(f"X  Failed: no trigger rate messages were received in {integration_time} seconds.")
+            self.finished.emit()
+            return
+
+        print("counts: ", trigcounts)
         rates = trigcounts / duration
+        print("rates: ", rates)
         disable = []
         enable = []
         too_many_triggers = 1.0  # i.e. 1.0 triggers per second or more is bad when x rays are off
@@ -86,13 +125,8 @@ class DisableHyperDialog(QtWidgets.QDialog):
             else:
                 disable.append(idx)
 
-        # 4) Return triggers to previous state (maybe zero them first?)
-        self.cursor.insertText("4) Data complete; stopping all triggers.\n")
-        self.zeroAllTriggers()
         nd = len(disable)
-        self.cursor.insertText(f"5) Disabling {nd} channels; sending edge triggers to all others.\n")
-        positive = self.positivePulseButton.isChecked()
-        threshold = self.levelSpinBox.value()
+        self.message.emit(f"4) Disabling {nd} channels; re-asserting edge triggers to all others.\n")
         if len(disable) > 0:
             ts = {
                 "ChannelIndices": disable,
@@ -106,9 +140,9 @@ class DisableHyperDialog(QtWidgets.QDialog):
                 "ChannelIndices": enable,
                 "AutoTrigger": False,
                 "EdgeTrigger": True,
-                "EdgeRising": positive,
-                "EdgeFalling": not positive,
-                "EdgeLevel": threshold,
+                "EdgeRising": self.positive,
+                "EdgeFalling": not self.positive,
+                "EdgeLevel": self.threshold,
                 "LevelTrigger": False,
                 }
             self.dcom.client.call("SourceControl.ConfigureTriggers", ts)
@@ -116,7 +150,8 @@ class DisableHyperDialog(QtWidgets.QDialog):
         if not self.save_quiet:
             delay = 5000 # ms
             QtCore.QTimer.singleShot(delay, self.endSilentTRIGGER)
-        self.cursor.insertText("Done! You may close this window.\n")
+        self.message.emit("Done! You may close this window.\n")
+        self.finished.emit()
 
     @pyqtSlot()
     def endSilentTRIGGER(self):
@@ -136,14 +171,12 @@ class DisableHyperDialog(QtWidgets.QDialog):
     def startEdgeTriggers(self, channels_to_configure):
         if len(channels_to_configure) == 0:
             return False
-        positive = self.positivePulseButton.isChecked()
-        threshold = self.levelSpinBox.value()
         config = {
             "ChannelIndices": channels_to_configure,
             "EdgeTrigger": True,
-            "EdgeRising": positive,
-            "EdgeFalling": not positive,
-            "EdgeLevel": threshold,
+            "EdgeRising": self.positive,
+            "EdgeFalling": not self.positive,
+            "EdgeLevel": self.threshold,
         }
         self.dcom.client.call("SourceControl.ConfigureTriggers", config)
         return True
