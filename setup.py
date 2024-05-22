9975b8699673c20b17e84136929b14424f8f9ba5
Joe Fowler <drjoefowler@gmail.com>
2024-05-22 13:46:44 -0600

    Change to v0.2.10

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=["dastardcommander"],
     test_suite="tests",
     url="https://github.com/usnistgov/dastardcommander",
-    version="0.2.9",
+    version="0.2.10",
     zip_safe=False,
     package_data={"dastardcommander": ["ui/*.ui", "ui/*.png"]},
     entry_points={
